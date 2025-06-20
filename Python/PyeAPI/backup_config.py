39ffcf41c53d891984bbf8d3799bc93f02286bf2
arista <atd-help@arista.com>
2025-06-20 09:59:16 +0000

    correction to backup_config

--- Python/PyeAPI/backup_config.py
+++ Python/PyeAPI/backup_config.py
@@ -3,24 +3,25 @@ import os
 import yaml
 
 pyeapi.load_config('eapi.conf')
+file = open ('switches.yml', 'r')
+switches_dict = yaml.safe_load(file)
 
 directory = "configs"
 exists = os.path.exists(directory)
 if not exists:
     os.makedirs(directory)
 
-    file = open ('swiches.yml', 'r')
-    switches_dict = yaml.safe_load(file)
-    for switch in switches_dict['device']:
+    
+for switch in switches_dict['device']:
     
 
-        connect = pyeapi.connect_to(switch)
-        running_config = connect.get_config(as_string='True')
-
+    connect = pyeapi.connect_to(switch)
+    running_config = connect.get_config(as_string='True')
+   
     
 
-        path = directory+'/'+switch+'.cfg'
-        file = open(path,'w')
-        file.write(running_config)
-        file.close()
-        print(f"Backing up {switch}")
+    path = directory+'/'+switch+'.cfg'
+    file = open(path,'w')
+    file.write(running_config)
+    file.close()
+    print(f"Backing up {switch}")
